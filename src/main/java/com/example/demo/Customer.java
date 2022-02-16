2f3d83a596a4d5219ce9f9ccef8df2a96d27e821
domca <domca.kamiencok@gmail.com>
2022-02-16 11:48:42 +0100

    naplnenie triedy Customer

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,4 +1,31 @@
 package com.example.demo;
 
 public class Customer {
+    private String firstname;
+    private String lastname;
+    private String contact;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }
