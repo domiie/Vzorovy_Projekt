a1ad6cfd8a67b7153697c830569068b5ee1ed307
slava <slavaborodin07@gmail.com>
2022-02-18 22:37:36 +0100

    borrowings

--- src/main/java/com/example/demo/BorrowedBook.java
+++ src/main/java/com/example/demo/BorrowedBook.java
@@ -2,29 +2,41 @@ package com.example.demo;
 
 public class BorrowedBook {
     private Long id;
-    private Customer borrower;
+    private Customer customer;
     private Book book;
 
     public Long getId() {
         return id;
     }
-
-    public Customer getBorrower() {
-        return borrower;
+    protected Customer getCustomer() {
+        return customer;
+    }
+    protected Book getBook() {
+        return book;
     }
 
-    public Book getBook() {
-        return book;
+    public Long getCustomerId(){
+        return customer.getId();
+    }
+    public String getCustomerName(){
+        return customer.getFirstName() + " " + customer.getLastName();
+    }
+    public Long getBookId(){
+        return book.getId();
+    }
+    public String getAutorName(){
+        return book.getAuthorFirstName() + " " + book.getAuthorLastName();
+    }
+    public String getTitle(){
+        return book.getTitle();
     }
 
     public void setId(Long id) {
         this.id = id;
     }
-
-    public void setBorrower(Customer borrower) {
-        this.borrower = borrower;
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
-
     public void setBook(Book book) {
         this.book = book;
     }
--- src/main/java/com/example/demo/BorrowingsController.java
+++ src/main/java/com/example/demo/BorrowingsController.java
@@ -4,39 +4,63 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class BorrowingsController {
     private List<BorrowedBook> borrowings;
     Customer customer;
     Book book;
-
     public BorrowingsController(){
         this.borrowings = init();
     }
-
+    //INIT
     private List<BorrowedBook> init(){
         List<BorrowedBook> borrowings = new ArrayList<>();
         BorrowedBook borrowing1 = new BorrowedBook();
-        borrowing1.setBorrower(customer);
+        //manualne objekty
+        customer = new Customer();
+        customer.setFirstName("Alena");
+        customer.setLastName("Dobra");
+        customer.setContact("a.dobra@example.com");
+        book = new Book();
+        book.setAuthorFirstName("Arthur");
+        book.setAuthorLastName("Doyle");
+        book.setTitle("Study in Red");
+        book.setIsbn("974-AD-41-C-F");
+        book.setBookCount(17);
+        //pridavame instancie BOOKa a CUSTOMERa do pozicky
+        borrowing1.setCustomer(customer);
         borrowing1.setBook(book);
         borrowings.add(borrowing1);
         borrowing1.setId((long) borrowings.indexOf(borrowing1));
         return borrowings;
     }
-
+    //GET BORROWING BY BOOK TITLE
     @GetMapping("/api/borrowings")
-    public List<BorrowedBook> getBorrowings(){
-        return this.borrowings;
-    }
+    public List<BorrowedBook> getBorrowingsByBookTitle(@RequestParam(required = false) String title){
 
-    //CREATE BORROWING
+        if(title == null){
+            for (BorrowedBook borrow : borrowings){
+                return this.borrowings;
+            }
+        }
+
+        List<BorrowedBook> filteredBorrowings = new ArrayList<>();
+
+        for (BorrowedBook borrow : borrowings){
+            if(borrow.getBook().getTitle().equals(title)){
+                filteredBorrowings.add(borrow);
+            }
+        }
+        return filteredBorrowings;
+    }
+    //CREATE NEW BORROWING
+    //iba testova metoda
     @PostMapping("/api/borrowings")
-    public String createBorrowings(@RequestBody BorrowedBook borrowing){
-        this.borrowings.add(borrowing);
-        borrowing.setId((long) borrowings.size()-1);
+    public String createBorrowing(@RequestBody BorrowedBook borrowedBook){
+        this.borrowings.add(borrowedBook);
+        borrowedBook.setId((long) borrowings.size()-1);
         return "Borrowing with id "+(this.borrowings.size()-1)+" was created.";
     }
-
-
 
 
 }
