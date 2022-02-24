5e8483f2f44bcbaef12ef8b803e82034192e9af5
Gelmoxx <djtoso@gmail.com>
2022-02-24 19:10:28 +0100

    Book Service - všetko hotové. Funguje tak ako predtým

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -8,38 +8,13 @@ import java.util.List;
 @RestController
 public class BookController {
 
-    private List<Book> books;
+    private BookService bookService;
 
-    public BookController(){
-        this.books = init();
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
-    private List<Book> init(){
-        List<Book> books = new ArrayList<>();
-
-        Book book1 = new Book();
-        book1.setAuthorFirstName("Arthur");
-        book1.setAuthorLastName("Doyle");
-        book1.setTitle("Study in Red");
-        book1.setIsbn("974-AD-41-C-F");
-        book1.setId(0);
-        book1.setBookCount(17);
-        books.add(book1);
-
-        Book book2 = new Book();
-        book2.setAuthorFirstName("J.R.R.");
-        book2.setAuthorLastName("Tolkien");
-        book2.setTitle("The Hobbit");
-        book2.setIsbn("128-XD-77-Q-F");
-        book2.setId(1);
-        book2.setBookCount(12);
-        books.add(book2);
-
-        return books;
-    }
-
-
-//    @GetMapping("/api/books")
+    //    @GetMapping("/api/books")
 //    public List<String> getBooksByTitle(@RequestParam(required = false) String title){
 //
 //        List<String> nonFilteredBooks = new ArrayList<>();
@@ -64,56 +39,31 @@ public class BookController {
     //Hľadanie knihy podľa názvu
     @GetMapping("/api/books")
     public List<Book> getBooksByTitle(@RequestParam(required = false) String title){
-
-        if(title == null){
-            for (Book book : books){
-                return this.books;
-            }
-        }
-
-        List<Book> filteredBooks = new ArrayList<>();
-
-        for (Book book : books){
-            if(book.getTitle().equals(title)){
-                filteredBooks.add(book);
-            }
-        }
-        return filteredBooks;
+        return bookService.getBooksByTitle(title);
     }
+
 
     //Hľadanie knihy podľa ID
     @GetMapping("/api/books/{bookId}")
     public List<Book> getBookById(@PathVariable Integer bookId){
-        List<Book> filteredBooks = new ArrayList<>();
-        for (Book book : books){
-            if(book.getId() == bookId){
-                filteredBooks.add(book);
-            }
-        }
-        return filteredBooks;
+        return bookService.getBookById(bookId);
     }
 
     //Pridanie novej knihy
     @PostMapping("/api/books")
     public Integer createBook(@RequestBody Book book){
-        this.books.add(book);
-        return this.books.size() - 1;
+        return bookService.createBook(book);
     }
 
     //Zmazanie knihy podľa ID
     @DeleteMapping("/api/books/{bookId}")
     public void deleteBook(@PathVariable Integer bookId){
-        this.books.remove(this.books.get(bookId));
+        bookService.deleteBook(bookId);
     }
 
     //Aktualizácia knihy podľa ID
     @PutMapping("/api/books/{bookId}")
     public void updateBook(@PathVariable Integer bookId, @RequestBody Book book){
-        this.books.get(bookId).setTitle(book.getTitle());
-        this.books.get(bookId).setAuthorFirstName(book.getAuthorFirstName());
-        this.books.get(bookId).setAuthorLastName(book.getAuthorLastName());
-        this.books.get(bookId).setBookCount(book.getBookCount());
-        this.books.get(bookId).setIsbn(book.getIsbn());
-        this.books.get(bookId).setId(book.getId());
+        bookService.updateBook(bookId, book);
     }
 }
---
+++ src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,88 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private List<Book> books;
+
+    public BookService(){
+        this.books = init();
+    }
+
+    private List<Book> init(){
+        List<Book> books = new ArrayList<>();
+
+        Book book1 = new Book();
+        book1.setAuthorFirstName("Arthur");
+        book1.setAuthorLastName("Doyle");
+        book1.setTitle("Study in Red");
+        book1.setIsbn("974-AD-41-C-F");
+        book1.setId(0);
+        book1.setBookCount(17);
+        books.add(book1);
+
+        Book book2 = new Book();
+        book2.setAuthorFirstName("J.R.R.");
+        book2.setAuthorLastName("Tolkien");
+        book2.setTitle("The Hobbit");
+        book2.setIsbn("128-XD-77-Q-F");
+        book2.setId(1);
+        book2.setBookCount(12);
+        books.add(book2);
+
+        return books;
+    }
+
+    public List<Book> getBooksByTitle(String title){
+        if(title == null){
+            for (Book book : books){
+                return this.books;
+            }
+        }
+
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books){
+            if(book.getTitle().equals(title)){
+                filteredBooks.add(book);
+            }
+        }
+        return filteredBooks;
+    }
+
+    public List<Book> getBookById(Integer bookId){
+        List<Book> filteredBooks = new ArrayList<>();
+        for (Book book : books){
+            if(book.getId() == bookId){
+                filteredBooks.add(book);
+            }
+        }
+        return filteredBooks;
+    }
+
+    public Integer createBook(Book book){
+        this.books.add(book);
+        return this.books.size() - 1;
+    }
+
+    public void deleteBook(Integer bookId){
+        this.books.remove(this.books.get(bookId));
+    }
+
+    public void updateBook(Integer bookId, Book book){
+        this.books.get(bookId).setTitle(book.getTitle());
+        this.books.get(bookId).setAuthorFirstName(book.getAuthorFirstName());
+        this.books.get(bookId).setAuthorLastName(book.getAuthorLastName());
+        this.books.get(bookId).setBookCount(book.getBookCount());
+        this.books.get(bookId).setIsbn(book.getIsbn());
+        this.books.get(bookId).setId(book.getId());
+    }
+
+}
