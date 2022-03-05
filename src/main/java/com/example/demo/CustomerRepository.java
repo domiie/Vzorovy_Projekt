328b8838630a5a98475b9c30912d278e3cb17c3c
domca <domca.kamiencok@gmail.com>
2022-03-05 16:43:13 +0100

    customer entity a repository, idk tbh

---
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
+    @Override
+    List<CustomerEntity> findAll();
+}
