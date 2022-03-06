77189da914b2e4aa7a28f935a7056ddf22938b22
slava <slavaborodin07@gmail.com>
2022-03-06 19:31:29 +0100

    uprava

--- src/main/java/com/example/demo/BookRepository.java
+++ src/main/java/com/example/demo/BookRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends CrudRepository<BookEntity, Long>  {
+    Optional<BookEntity> findByTitle(String title);
 }
 
