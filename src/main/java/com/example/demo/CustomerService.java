05609a5da24579d9e93ffdd235cf6eefe75178c6
domca <domca.kamiencok@gmail.com>
2022-03-06 11:21:19 +0100

    dorobene metody v CustomerService

--- src/main/java/com/example/demo/CustomerService.java
+++ src/main/java/com/example/demo/CustomerService.java
@@ -1,80 +1,73 @@
 package com.example.demo;
 
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class CustomerService {
-    protected List<Customer> customers;
+    private final CustomerRepository customerRepository;
 
-    public CustomerService(){
-        this.customers = init();
+    public CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
     }
 
-    //INITIALIZE
-    private List<Customer> init(){
-        List<Customer> customers = new ArrayList<>();
+    private static Customer mapToCustomer(CustomerEntity customerEntity){
+        Customer customer = new Customer();
 
-        Customer customer1 = new Customer();
-        customer1.setFirstName("Alena");
-        customer1.setLastName("Dobra");
-        customer1.setContact("a.dobra@example.com");
-        customers.add(customer1);
-        customer1.setId((long) customers.indexOf(customer1));
+        customer.setFirstName(customerEntity.getFirstname());
+        customer.setLastName(customerEntity.getLastname());
+        customer.setContact(customerEntity.getContact());
 
-        Customer customer2 = new Customer();
-        customer2.setFirstName("Ivan");
-        customer2.setLastName("Lexa");
-        customer2.setContact("ivan.lexa@example.com");
-        customers.add(customer2);
-        customer2.setId((long) customers.indexOf(customer2));
-
-        return customers;
+        return customer;
     }
-
-    public List<Customer> getListOfCustomers(){
-          return this.customers;
-     }
 
     //CREATE
-    public String createCustomer(Customer customer){
-        this.customers.add(customer);
-        customer.setId((long) customers.size()-1);
-        return "Customer with id "+(this.customers.size()-1)+" created.";
+    @Transactional
+    public Long createCustomer(Customer customer){
+        CustomerEntity customerEntity = new CustomerEntity();
+
+        customerEntity.setFirstname(customer.getFirstName());
+        customerEntity.setLastname(customer.getLastName());
+        customerEntity.setContact(customer.getContact());
+
+        this.customerRepository.save(customerEntity);
+
+        return customerEntity.getId();
     }
 
     //LIST CUSTOMERS
+    @Transactional
     public List<Customer> getCustomers(String lastname){
-        if(lastname == null){
-            return this.customers;
+        List<Customer> cust = new LinkedList<>();
+        for(CustomerEntity c1 : customerRepository.findAll()){
+            Customer c2 = mapToCustomer(c1);
+            cust.add(c2);
         }
-
-        List<Customer> filteredCustomers = new ArrayList<>();
-        for(Customer customer : customers){
-            if(customer.getLastName().equals(lastname)){
-                filteredCustomers.add(customer);
-            }
-        }
-        return filteredCustomers;
+        return cust;
     }
 
     //GET CUSTOMER BY ID
+    @Transactional
     public Customer getCustomer(Integer customerId){
-        return this.customers.get(customerId);
+        //TODO DOROBIT
+        return  null;
     }
 
     //UPDATE CUSTOMER
+    @Transactional
     public void updateCustomer(Integer customerId, Customer customer){
-        this.customers.get(customerId).setFirstName(customer.getFirstName());
-        this.customers.get(customerId).setLastName(customer.getLastName());
-        this.customers.get(customerId).setContact(customer.getContact());
+        //TODO DOROBIT
     }
 
     //DELETE CUSTOMER
+    @Transactional
     public void deleteCustomer(Integer customerId){
-        this.customers.remove(this.customers.get(customerId));
+        //TODO DOROBIT
     }
 
 }
